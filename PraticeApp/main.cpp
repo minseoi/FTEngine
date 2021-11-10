15be6fad801587dd16ff3be70095f6b3f0db9587
minseoi <minseoi.dev@gmail.com>
2021-11-10 22:23:26 +0900

    Initial Commit

---
+++ PraticeApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PraticeApp
+//
+//  Created by minseoi on 2021/11/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
